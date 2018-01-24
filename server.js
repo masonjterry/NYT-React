c790f9b180d64320b43028721d0942a627196d3b
Mason Terry <masonjterry@Masons-Air.attlocal.net>
2018-01-24 17:50:50 -0600

    initial files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`Listening on ${PORT}`);
+})
